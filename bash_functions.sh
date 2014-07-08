52a1e402ef297c1d8334252be9bdbc2bec0f3dec
Tyler A Land <tland9@uw.edu>
2014-07-08 11:13:50 -0700

    Don't quit bash on error

--- bash_functions.sh
+++ bash_functions.sh
@@ -4,7 +4,7 @@ function diffhead {
     diff=vimdiff
     if [[ $# -ne 3 ]]; then
         echo "Usage: diffhead file1 file2 head_length"
-        exit $E_BADARGS
+        return $E_BADARGS
     fi
     if [[ -x "$(type -p $diff)" ]]; then
         $diff <(head -n $3 $1) <(head -n $3 $2)
