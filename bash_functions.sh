f0caf62de8a93683ab3692a2808da034e3676fc4
Tyler A Land <tyleraland@gmail.com>
2014-11-08 19:04:05 -0800

    custom prompt with color

--- bash_functions.sh
+++ bash_functions.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+##### Colors #######
+COLOR_NONE="\[\e[0m\]"
+RED="\[\033[0;31m\]"
+GREEN="\[\033[0;32m\]"
+PURPLE="\[\033[0;35\]"
+LIGHT_GRAY="\[\033[0;37m\]"
+LIGHT_RED="\[\033[1;31m\]"
+LIGHT_GREEN="\[\033[1;32m\]"
+YELLOW="\[\033[1;33m\]"
+BLUE="\[\033[1;34m\]"
+LIGHT_PURPLE="\[\033[1;35m\]"
+WHITE="\[\033[1;37m\]"
+#####################
+
 function diffhead {
     diff=vimdiff
     if [[ $# -ne 3 ]]; then
@@ -38,4 +52,68 @@ function dt {
 #        target=${target}.${inc}
 #    fi
     echo $target
+}
+
+function is_git_repository {
+   git branch > /dev/null 2>&1
+}
+function set_git_branch {
+   # Set the final branch string
+   BRANCH=`parse_git_branch`
+   local TIME=`fmt_time` # format time for prompt string
+ }
+
+ function parse_git_branch() {
+   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+ }
+
+ function parse_git_dirty() {
+   [[ $(git status 2> /dev/null | tail -n1) != *"working directory clean"* ]] && echo "*"
+ }
+
+fmt_time () { #format time just the way I likes it
+    if [ `date +%p` = "PM" ]; then
+        meridiem="pm"
+    else
+        meridiem="am"
+    fi
+    date +"%l:%M:%S$meridiem"|sed 's/ //g'
+}
+
+# Return the prompt symbol to use, colorized based on the return value of the
+# previous command.
+function set_prompt_symbol () {
+  if test $1 -eq 0 ; then
+      PROMPT_SYMBOL="\$"
+  else
+      PROMPT_SYMBOL="${LIGHT_RED}\$${COLOR_NONE}"
+  fi
+}
+
+# Determine active Python virtualenv details.
+function set_virtualenv () {
+  if test -z "$VIRTUAL_ENV" ; then
+      PYTHON_VIRTUALENV=""
+  else
+      PYTHON_VIRTUALENV="${BLUE}[`basename \"$VIRTUAL_ENV\"`]${BLUE} "
+  fi
+}
+
+# Set the full bash prompt.
+function set_bash_prompt () {
+  # Set the PROMPT_SYMBOL variable. We do this first so we don't lose the
+  # return value of the last command.
+  set_prompt_symbol $?
+
+  # Set the PYTHON_VIRTUALENV variable.
+  set_virtualenv
+
+  # Set the BRANCH variable.
+  if is_git_repository ; then
+    set_git_branch
+  else
+    BRANCH=''
+  fi
+  # Set the bash prompt variable.
+  PS1="${PYTHON_VIRTUALENV}${GREEN}\w${RED} ${BRANCH}${PROMPT_SYMBOL}${COLOR_NONE} "
 }
--- bashrc
+++ bashrc
@@ -2,6 +2,14 @@
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
 
+if [ -f ~/.bash_functions.sh ]; then
+    source ~/.bash_functions.sh
+fi
+
+if [ -f ~/.bash_local.sh ]; then
+    source ~/.bash_local.sh
+fi
+
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
 
@@ -53,21 +61,29 @@ if [ -n "$force_color_prompt" ]; then
     fi
 fi
 
-if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
-fi
-unset color_prompt force_color_prompt
 
-# If this is an xterm set the title to user@host:dir
-case "$TERM" in
-xterm*|rxvt*)
-    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-    ;;
-*)
-    ;;
-esac
+#### Color Prompt ####
+# if [ "$color_prompt" = yes ]; then
+#     PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+# else
+#     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+# fi
+# unset color_prompt force_color_prompt
+# 
+# # If this is an xterm set the title to user@host:dir
+# case "$TERM" in
+# xterm*|rxvt*)
+#     PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+#     ;;
+# *)
+#     ;;
+# esac
+
+# Green/blue prompt for regular users
+PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\e[1;37m\]'
+PROMPT_COMMAND=set_bash_prompt
+
+#######################
 
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
@@ -120,8 +136,6 @@ if [[ -f ~/.git-completion.sh ]]; then
     source ~/.git-completion.sh
 fi
 
-# Green/blue prompt for regular users
-PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\e[1;37m\]'
 
 # PATHs
 
@@ -194,12 +208,3 @@ ssh-refresh() {
     export DISPLAY=$NEW_DISPLAY
     fi
 }
-
-if [ -f ~/.bash_functions.sh ]; then
-    source ~/.bash_functions.sh
-fi
-
-if [ -f ~/.bash_local.sh ]; then
-    source ~/.bash_local.sh
-fi
-
